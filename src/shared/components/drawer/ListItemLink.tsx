4e404aa3df73e8941805c66692e0458ffdd79802
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-05-22 17:54:24 -0300

    feat: editando icone do drawer

--- src/shared/components/drawer/ListItemLink.tsx
+++ src/shared/components/drawer/ListItemLink.tsx
@@ -23,10 +23,34 @@ export const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, on
     };
 
     return (
-        <ListItemButton selected={!!math} onClick={handleClick}>
-            <ListItemIcon>
+        <ListItemButton selected={!!math} onClick={handleClick}
+            sx={{
+                '&.Mui-selected': {
+                    backgroundColor: 'rgba(59, 130, 246, 0.2)', // bg-blue-500/20
+                    borderColor: '#2563eb', // border-blue-600
+                    color: '#ffffff' // text-white
+                },
+                '&:hover': {
+                    backgroundColor: 'rgba(59, 130, 246, 0.2)', // bg-blue-500/20
+                    borderColor: '#2563eb', // border-blue-600
+                },
+                backgroundColor: 'rgba(107, 114, 128, 0.2)', // bg-gray-500/20
+                borderColor: '#000000', // border-black
+                color: '#ffffff', // text-white
+                // padding: '8px', // p-2
+                // marginY: '8px', // my-2
+                borderRadius: '5px', // rounded-[5px]
+                borderWidth: '2px', // border-2
+                textAlign: 'center', // text-center
+                fontWeight: '600', // font-semibold
+                fontSize: '16px', // text-[16px]
+                marginBottom: '13px',
+                // width: '100%' // full width
+            }}
+        >
+            {/* <ListItemIcon>
                 <Icon>{icon}</Icon>
-            </ListItemIcon>
+            </ListItemIcon> */}
             <ListItemText primary={label} />
         </ListItemButton>
     );
