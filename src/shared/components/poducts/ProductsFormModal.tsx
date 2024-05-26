e3681bd473240ee4bde542dd3679c9d446e3f65f
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-05-26 14:04:44 -0300

    feat: validações

--- src/shared/components/poducts/ProductsFormModal.tsx
+++ src/shared/components/poducts/ProductsFormModal.tsx
@@ -13,27 +13,34 @@ import {
     Typography,
     Box
 } from '@mui/material';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { INewProduct } from '../../../@types/IApiResponseProducts'; 
 import { ICategory } from '../../../@types/IApiResponseCategories';
 import { CategoriesService } from '../../services/api/categories/Categories';
 import { SuppliersService } from '../../services/api/suppliers/SuppliersService';
 import { ISupplier } from '../../../@types/ISupplier';
 import { IProductFormModalProps } from '../../../@types/IProductFormModalProps'; 
+import { AlertDinamic } from '../alert/AlertDinamic';
+
+
+const validationSchema = yup.object({
+    name: yup.string().required('Nome é obrigatório'),
+    purchase_price: yup.number().required('Preço de compra é obrigatório').positive('Deve ser um número positivo'),
+    quantity: yup.number().required('Quantidade é obrigatória').min(1, 'A quantidade deve ser pelo menos 1'),
+    sale_price: yup.number().required('Preço de venda é obrigatório').positive('Deve ser um número positivo'),
+    category_id: yup.number().required('Categoria é obrigatória').min(1, 'Selecione uma categoria'),
+    supplier_id: yup.number().required('Fornecedor é obrigatório').min(1, 'Selecione um fornecedor')
+});
 
 const ProductsFormModal: React.FC<IProductFormModalProps> = ({ open, onClose, onSave }) => {
     const [categories, setCategories] = useState<ICategory[]>([]);
     const [error, setError] = useState<string | null>(null);
     const [suppliers, setSuppliers] = useState<ISupplier[]>([]);
-    const [product, setProduct] = useState<INewProduct>({
-        name: '',
-        purchase_price: 0,
-        quantity: 0,
-        sale_price: 0,
-        category_id: 0,
-        supplier_id: 0
-    });
-    const [newCategory, setNewCategory] = useState<string>('');
     const [showNewCategoryField, setShowNewCategoryField] = useState<boolean>(false);
+    const [newCategory, setNewCategory] = useState<string>('');
+    const [alertMessage, setAlertMessage] = useState<string | null>(null);
+    const [alertSeverity, setAlertSeverity] = useState<'success' | 'error' | undefined>(undefined);
 
     useEffect(() => {
         const fetchCategories = async () => {
@@ -58,136 +65,180 @@ const ProductsFormModal: React.FC<IProductFormModalProps> = ({ open, onClose, on
         fetchSuppliers();
     }, []);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | { target: { name: string; value: any; } }) => {
-        const { name, value } = e.target;
-        setProduct(prev => ({ ...prev, [name]: value }));
-    };
-
-    const handleSave = () => {
-        onSave(product);
-        onClose();
-    };
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            purchase_price: 0,
+            quantity: 0,
+            sale_price: 0,
+            category_id: 0,
+            supplier_id: 0
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            setAlertMessage("Produto cadastrado com sucesso!");
+            setAlertSeverity("success");
+            onSave(values);
+            onClose();
+        }
+    });
 
     const handleAddCategory = () => {
-        // Implementar a lógica para adicionar uma nova categoria
         console.log('Nova categoria:', newCategory);
         setShowNewCategoryField(false);
         setNewCategory('');
     };
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
+    const handleSubmit = async () => {
+        setAlertMessage(null);
+        try {
+            await validationSchema.validate(formik.values, { abortEarly: false });
+            formik.handleSubmit();
+        } catch (err) {
+            if (err instanceof yup.ValidationError) {
+                setAlertMessage("Erro ao cadastrar produto, revise os campos!");
+                setAlertSeverity("error");
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (alertMessage) {
+            const timer = setTimeout(() => {
+                setAlertMessage(null);
+            }, 3000);
+            return () => clearTimeout(timer);
+        }
+    }, [alertMessage]);
+
 
     return (
-        <Dialog open={open} onClose={onClose} PaperProps={{ sx: { width: '600px', maxHeight: '90vh', backgroundColor: '#10141E', color: '#FFFFFF' } }}>
-            <DialogTitle>Cadastrar Produtos</DialogTitle>
-            <DialogContent>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    name="name"
-                    label="Nome"
-                    type="text"
-                    fullWidth
-                    value={product.name}
-                    onChange={handleChange}
-                    sx={{label: { color: 'black' }, backgroundColor: '#E8F0F3', borderRadius: '12px' }}
-                />
-                <TextField
-                    margin="dense"
-                    name="purchase_price"
-                    label="Preço de Compra"
-                    type="number"
-                    fullWidth
-                    value={product.purchase_price}
-                    onChange={handleChange}
-                    sx={{label: { color: 'black' }, backgroundColor: '#E8F0F3', borderRadius: '12px', marginTop: '18px'  }}
-                />
-                <TextField
-                    margin="dense"
-                    name="quantity"
-                    label="Quantidade"
-                    type="number"
-                    fullWidth
-                    value={product.quantity}
-                    onChange={handleChange}
-                    sx={{label: { color: 'black' }, backgroundColor: '#E8F0F3', borderRadius: '12px', marginTop: '18px'  }}
-                />
-                <TextField
-                    margin="dense"
-                    name="sale_price"
-                    label="Preço de Venda"
-                    type="number"
-                    fullWidth
-                    value={product.sale_price}
-                    onChange={handleChange}
-                    sx={{label: { color: 'black' }, backgroundColor: '#E8F0F3', borderRadius: '12px', marginTop: '18px'  }}
-                />
-                <FormControl fullWidth sx={{ label: { color: 'black' }, marginTop: '18px', backgroundColor: '#E8F0F3', borderRadius: '12px' }}>
-                    <InputLabel sx={{ color: '#FFFFFF' }}>Categoria</InputLabel>
-                    <Select
-                        name="category_id"
-                        value={product.category_id}
-                        onChange={handleChange}
-                        sx={{ label: { color: 'black' }, color: '#FFFFFF' }}
-                    >
-                        {categories.map(category => (
-                            <MenuItem key={category.id} value={category.id}>
-                                {category.name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                    <Box mt={1}>
-                        <Box mt={1} display="flex" justifyContent="flex-end">
-                            <Typography variant="body2" sx={{ color: 'black' , cursor: 'pointer' }} onClick={() => setShowNewCategoryField(!showNewCategoryField)}>
-                                {showNewCategoryField ? 'Cancelar' : 'Não encontrou a categoria desejada?'}
-                            </Typography>
-                        </Box>
-      
-                        {showNewCategoryField && (
-                            <Box mt={1} display="flex">
-                                <TextField
-                                    margin="dense"
-                                    label="Nova Categoria"
-                                    type="text"
-                                    // fullWidth
-                                    value={newCategory}
-                                    onChange={(e) => setNewCategory(e.target.value)}
-                                    sx={{ input: { color: 'black' }, label: { color: 'black' }, backgroundColor: '#E8F0F3', borderRadius: '12px' }}
-                                />
-                                <Button onClick={handleAddCategory} variant="outlined" sx={{ marginLeft: 1, height: 50, width: 100, marginTop: 1.4 }}>
-                                    Adicionar
-                                </Button>
+        <>
+            {alertMessage && <AlertDinamic message={alertMessage} severityTipo={alertSeverity} />}
+            <Dialog open={open} onClose={onClose} PaperProps={{ sx: { width: '600px', maxHeight: '90vh', backgroundColor: '#10141E', color: '#FFFFFF' } }}>
+                <DialogTitle>Cadastrar Produtos</DialogTitle>
+                <form onSubmit={formik.handleSubmit}>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            name="name"
+                            label="Nome"
+                            type="text"
+                            fullWidth
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.name && Boolean(formik.errors.name)}
+                            helperText={formik.touched.name && formik.errors.name}
+                            sx={{label: { color: 'black' }, backgroundColor: '#E8F0F3', borderRadius: '12px' }}
+                        />
+                        <TextField
+                            margin="dense"
+                            name="purchase_price"
+                            label="Preço de Compra"
+                            type="number"
+                            fullWidth
+                            value={formik.values.purchase_price}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.purchase_price && Boolean(formik.errors.purchase_price)}
+                            helperText={formik.touched.purchase_price && formik.errors.purchase_price}
+                            sx={{label: { color: 'black' }, backgroundColor: '#E8F0F3', borderRadius: '12px', marginTop: '18px'  }}
+                        />
+                        <TextField
+                            margin="dense"
+                            name="quantity"
+                            label="Quantidade"
+                            type="number"
+                            fullWidth
+                            value={formik.values.quantity}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.quantity && Boolean(formik.errors.quantity)}
+                            helperText={formik.touched.quantity && formik.errors.quantity}
+                            sx={{label: { color: 'black' }, backgroundColor: '#E8F0F3', borderRadius: '12px', marginTop: '18px'  }}
+                        />
+                        <TextField
+                            margin="dense"
+                            name="sale_price"
+                            label="Preço de Venda"
+                            type="number"
+                            fullWidth
+                            value={formik.values.sale_price}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.sale_price && Boolean(formik.errors.sale_price)}
+                            helperText={formik.touched.sale_price && formik.errors.sale_price}
+                            sx={{label: { color: 'black' }, backgroundColor: '#E8F0F3', borderRadius: '12px', marginTop: '18px'  }}
+                        />
+                        <FormControl fullWidth sx={{ marginTop: '18px', backgroundColor: 'gray', borderRadius: '12px' }}>
+                            <InputLabel sx={{ color: '#FFFFFF' }}>Categoria</InputLabel>
+                            <Select
+                                name="category_id"
+                                value={formik.values.category_id}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                error={formik.touched.category_id && Boolean(formik.errors.category_id)}
+                                sx={{ color: '#FFFFFF' }}
+                            >
+                                {categories.map(category => (
+                                    <MenuItem key={category.id} value={category.id}>
+                                        {category.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                            <Box mt={1} display="flex" justifyContent="flex-end">
+                                <Typography variant="body2" sx={{ color: '#FFFFFF', cursor: 'pointer' }} onClick={() => setShowNewCategoryField(!showNewCategoryField)}>
+                                    {showNewCategoryField ? 'Cancelar' : 'Não encontrou a categoria desejada?'}
+                                </Typography>
                             </Box>
-                        )}
-                    </Box>
-                </FormControl>
-                <FormControl fullWidth sx={{label: { color: 'black' }, marginTop: '18px', backgroundColor: '#E8F0F3', borderRadius: '12px' }}>
-                    <InputLabel sx={{ color: '#FFFFFF' }}>Fornecedor</InputLabel>
-                    <Select
-                        name="supplier_id"
-                        value={product.supplier_id}
-                        onChange={handleChange}
-                        sx={{ color: '#FFFFFF' }}
-                    >
-                        {suppliers.map(supplier => (
-                            <MenuItem key={supplier.id} value={supplier.id}>
-                                {supplier.name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-            </DialogContent>
-            <DialogActions>
-                <Button variant='outlined' onClick={onClose} color="error">
-                    Cancelar
-                </Button>
-                <Button variant='outlined' onClick={handleSave} color="primary">
-                    Salvar
-                </Button>
-            </DialogActions>
-        </Dialog>
+                            {showNewCategoryField && (
+                                <Box mt={1} display="flex">
+                                    <TextField
+                                        margin="dense"
+                                        label="Nova Categoria"
+                                        type="text"
+                                        fullWidth
+                                        value={newCategory}
+                                        onChange={(e) => setNewCategory(e.target.value)}
+                                        sx={{ input: { color: '#FFFFFF' }, label: { color: '#FFFFFF' }, backgroundColor: 'gray', borderRadius: '12px' }}
+                                    />
+                                    <Button onClick={handleAddCategory} variant="outlined" sx={{ marginLeft: 1 }}>
+                                        Adicionar
+                                    </Button>
+                                </Box>
+                            )}
+                        </FormControl>
+                        <FormControl fullWidth sx={{ marginTop: '18px', backgroundColor: 'gray', borderRadius: '12px' }}>
+                            <InputLabel sx={{ color: '#FFFFFF' }}>Fornecedor</InputLabel>
+                            <Select
+                                name="supplier_id"
+                                value={formik.values.supplier_id}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                error={formik.touched.supplier_id && Boolean(formik.errors.supplier_id)}
+                                sx={{ color: '#FFFFFF' }}
+                            >
+                                {suppliers.map(supplier => (
+                                    <MenuItem key={supplier.id} value={supplier.id}>
+                                        {supplier.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant='outlined' onClick={onClose} color="error">
+                            Cancelar
+                        </Button>
+                        <Button variant='outlined' type="button" onClick={handleSubmit} color="primary">
+                            Salvar
+                        </Button>
+                    </DialogActions>
+                </form>
+            </Dialog>
+        </>
     );
 };
 
